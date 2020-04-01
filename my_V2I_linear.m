115e12e96f799171cc552223fe6297eebb1672e3
Charles Chen <cccharleschan@utexas.edu>
2020-03-31 18:44:44 -0700

    Update comments

--- my_V2I_linear.m
+++ my_V2I_linear.m
@@ -1,7 +1,11 @@
 function [ I ] = my_V2I_linear(V, Z)
-%Calculate voltage waveform from current waveform
-V_hat = fft(V);
-I_hat = V_hat./conj(Z);
-I = real(ifft(I_hat));
+%Compute current response of the linear electrode
+%   Input: V - Nx1 vector, periodic voltage waveform of the
+%   electrode-electrolyte interface; Z - Nx1 vector, impedance spectrum of
+%   the interface, impedances at f = (0:N-1)/N/T.
+%   Output: I - Nx1 vector, the calculated current response.
+V_hat = fft(V); %Fast Fourier transform (FFT) to frequency domain
+I_hat = V_hat./conj(Z); %Apply the impedance spectrum
+I = real(ifft(I_hat)); %Inverse FFT back to time domain
 end
 
--- my_V2I_nonlin.m
+++ my_V2I_nonlin.m
@@ -1,7 +1,11 @@
 function [ I ] = my_V2I_nonlin( V, Diode, Iphoton )
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
-I = -interp1(Diode.V, Diode.I, V, 'linear', 'extrap');
-I = I + Iphoton;
+%Compute current response of the nonlinear circuit driver
+%   Input: V - Nx1 vector, voltage waveform at the port of the circuit
+%   driver; Diode - dark IV curve of the photodiode that drives the
+%   circuit; Iphoton - Nx1 vector, photocurrent induced by the pulsed
+%   laser illumination.
+%   Output: I - current output of the circuit driver.
+I = -interp1(Diode.V, Diode.I, V, 'linear', 'extrap'); %p->n current under forward bias 
+I = I + Iphoton; %total current output
 end
 
